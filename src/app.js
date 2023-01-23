794dc479287b12e517697ab0341dd1a52c054fb8
Otto777L <otto.lh12@gmail.com>
2023-01-23 21:21:45 +0000

    Hack Domain Added

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["rates", "bus", "lectores", "jesus"];
+  let extensions = [".com", ".net", ".us", ".io", ".org", ".es"];
+
+  let domainName = [];
+
+  for (let item of pronoun) {
+    for (let item2 of adj) {
+      for (let item3 of noun) {
+        for (const item4 of extensions) {
+          if (item3.slice(-2) == item4.substring(1)) {
+            domainName = `${item}${item2}${item3.slice(0, -2)}.${item3.slice(
+              -2
+            )}`;
+          } else {
+            domainName = `${item}${item2}${item3}${item4}`;
+          }
+          console.log(domainName);
+        }
+      }
+    }
+  }
 };
